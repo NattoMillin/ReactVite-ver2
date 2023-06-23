793c6efecdae46cdc4acd3789b5aae5ea7ec383b
Natto Millin <zz112ggww@icloud.com>
2023-06-23 07:09:56 +0000

    Please!!

--- vite-survey-app/src/App.tsx
+++ vite-survey-app/src/App.tsx
@@ -1,9 +1,32 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import   Home   from './components/Home';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Loading } from './components/Loading';
+import { generateEnv } from './generateEnv';
+import liff from '@line/liff';
 
 function App() {
+
+  const {liffId, mock} = generateEnv();
+  
+  const opstion = {liffId: liffId, mock: mock}
+
+  useEffect(() => {
+    liff
+    .init(opstion)
+    .then(async () => {
+      console.log("init collect")
+      if (!liff.isLoggedIn()) {
+        console.log("login start")
+        await liff.login();
+        console.log("login collect")
+      }
+  })
+  .catch((e) => {
+    alert(`LIFF error: ${e.message}`)
+  })
+  }, [])
+
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
--- vite-survey-app/src/main.tsx
+++ vite-survey-app/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css'
 import App from './App'
 import liff from '@line/liff'
-import {generateEnv} from './generateEnv';
+// import {generateEnv} from './generateEnv';
 import LiffMockPlugin from '@line/liff-mock';
 import { createRoot } from 'react-dom/client'
 
@@ -11,10 +11,7 @@ if (import.meta.env.VITE_NODE_ENV !== 'production') {
   // https://github.com/line/liff-mock
 }
 
-const {liffId, mock} = generateEnv();
-
-const opstion = {liffId: liffId, mock: mock}
-
+/*
 liff
   .init(opstion)
   .then(async () => {
@@ -37,3 +34,13 @@ liff
   .catch((e) => {
     alert(`LIFF error: ${e.message}`)
   })
+  */
+
+const container = document.getElementById('root')
+console.log("root find")
+if (!container) throw new Error('Failed to find the root element')
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
