3a72945aaaf6c97b63b3096f885c1e15cc550f41
Natto Millin <zz112ggww@icloud.com>
2023-06-23 07:12:25 +0000

    cannot deploy

--- vite-survey-app/vite.config.ts
+++ vite-survey-app/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './',    
+  base: './',
   publicDir: 'public',
   plugins: [react()],
   build: {
