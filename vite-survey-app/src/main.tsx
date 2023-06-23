bf4e4f4a7a0d3533ab4bc83c53f64dad65f7d47c
Natto Millin <zz112ggww@icloud.com>
2023-06-23 05:47:47 +0000

    commit first create

--- vite-survey-app/src/main.tsx
+++ vite-survey-app/src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App'
+import liff from '@line/liff'
+import {generateEnv} from './generateEnv';
+import LiffMockPlugin from '@line/liff-mock';
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (import.meta.env.VITE_NODE_ENV !== 'production') {
+  liff.use(new LiffMockPlugin());
+  // https://github.com/line/liff-mock
+}
+
+const {liffId, mock} = generateEnv();
+
+const opstion = {liffId: liffId, mock: mock}
+
+liff
+  .init(opstion)
+  .then(async () => {
+    if (!liff.isLoggedIn()) {
+      await liff.login();
+    }
+    const container = document.getElementById('root')
+    if (!container) throw new Error('Failed to find the root element')
+    createRoot(container).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    )
+  })
+  .catch((e) => {
+    alert(`LIFF error: ${e.message}`)
+  })
