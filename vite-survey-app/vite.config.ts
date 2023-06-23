b1d7e1927d0f93753a6be33c7d88c3237a666298
Natto Millin <zz112ggww@icloud.com>
2023-06-23 06:42:56 +0000

    errorrrorrororororo

--- vite-survey-app/vite.config.ts
+++ vite-survey-app/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './',    
-  base: process.env.Vite_GITHUB_PAGES // この行を追加
-  ? "/ReactVite-ver2/"            // この行を追加
-  : "/ReactVite-ver2/",                     // この行を追加
   publicDir: 'public',
   plugins: [react()],
   build: {
@@ -15,3 +12,9 @@ export default defineConfig({
   },
   envDir: './',
 })
+
+/*
+base: process.env.Vite_GITHUB_PAGES // この行を追加
+? "/ReactVite-ver2/"            // この行を追加
+: "/ReactVite-ver2/", 
+*/
