01fb10517a36835f83c8f10b8dc52c95b934f09d
Natto Millin <zz112ggww@icloud.com>
2023-06-23 04:57:18 +0000

    first commit

---
+++ vite-survey-app/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: './',    
+  base: process.env.GITHUB_PAGES  // この行を追加
+  ? "ReactVite-ver2"            // この行を追加
+  : "./",                     // この行を追加
+  publicDir: 'public',
+  plugins: [react()],
+  build: {
+    // `root` からの相対パスで指定する
+    outDir: '../dist',
+  },
+  envDir: './',
+})
