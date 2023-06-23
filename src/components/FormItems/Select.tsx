4fde526f2a704a843f237839b7ba292bc731d50a
Natto Millin <zz112ggww@icloud.com>
2023-06-23 08:04:37 +0000

    ikaga

--- src/components/FormItems/Select.tsx
+++ src/components/FormItems/Select.tsx
@@ -5,7 +5,7 @@ import { ItemTypes } from '../../types/type';
 export const Options = (props: ItemTypes & UseControllerProps<FormValues>) => {
     const { field } = useController(props);
     return (
-        <label>
+        <label> 
             {field.name}
             <select name={field.name} ref={field.ref} onChange={field.onChange} onBlur={field.onBlur}>
                 {props.Items ? props.Items.map(item => <option key={item} value={item}>{item}</option>) : null}
